dd11452059e4e4d1473ab7c24de0010a4fd8c74e
Libraum <wmchoward@internode.on.net>
2024-07-22 14:59:51 +0930

    Commit:

--- src/main/java/net/libraum/platypus/block/custom/PlatyplushieBlock.java
+++ src/main/java/net/libraum/platypus/block/custom/PlatyplushieBlock.java
@@ -3,17 +3,29 @@ package net.libraum.platypus.block.custom;
 import net.libraum.platypus.block.entity.PlatyplushieBlockEntity;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RotationPropertyHelper;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.core.animatable.GeoAnimatable;
+import software.bernie.geckolib.core.animation.AnimatableManager;
+import software.bernie.geckolib.core.animation.AnimationController;
+import software.bernie.geckolib.core.animation.AnimationState;
+import software.bernie.geckolib.core.object.PlayState;
 
 public class PlatyplushieBlock extends BlockWithEntity {
     public static final IntProperty ROTATION = Properties.ROTATION;
@@ -41,6 +53,18 @@ public class PlatyplushieBlock extends BlockWithEntity {
     @Override
     public BlockState mirror(BlockState state, BlockMirror mirror) {
         return state.with(ROTATION, Integer.valueOf(mirror.mirror((Integer)state.get(ROTATION), 16)));
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient) {
+            if (hand == Hand.MAIN_HAND) {
+                PlatyplushieBlockEntity platyplushieBlockEntity = (PlatyplushieBlockEntity) world.getBlockEntity(pos);
+                platyplushieBlockEntity.triggerAnim("bounce_controller", "bounce");
+                world.playSound(null, pos, SoundEvents.ENTITY_AXOLOTL_IDLE_AIR, SoundCategory.BLOCKS, 1f, 1.5f);
+            }
+        }
+        return ActionResult.SUCCESS;
     }
 
     @Override
--- src/main/java/net/libraum/platypus/block/entity/PlatyplushieBlockEntity.java
+++ src/main/java/net/libraum/platypus/block/entity/PlatyplushieBlockEntity.java
@@ -1,31 +1,33 @@
 package net.libraum.platypus.block.entity;
 
+import net.libraum.platypus.block.custom.PlatyplushieBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import software.bernie.geckolib.animatable.GeoBlockEntity;
+import software.bernie.geckolib.animatable.SingletonGeoAnimatable;
 import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
-import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.*;
 import software.bernie.geckolib.core.object.PlayState;
+import software.bernie.geckolib.util.GeckoLibUtil;
 import software.bernie.geckolib.util.RenderUtils;
 
 public class PlatyplushieBlockEntity extends BlockEntity implements GeoBlockEntity {
-    private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
+    private static final RawAnimation BOUNCE = RawAnimation.begin().thenPlay("animation.platyplushie_block.bounce");
+    private final AnimatableInstanceCache cache = GeckoLibUtil.createInstanceCache(this);
 
     public PlatyplushieBlockEntity( BlockPos pos, BlockState state) {
         super(ModBlockEntities.PLATYPLUSHIE_BLOCK_ENTITY, pos, state);
+
+        SingletonGeoAnimatable.registerSyncedAnimatable(this);
     }
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
-        controllers.add(new AnimationController<>(this, "controller", 0, this::predicate));
-    }
-
-    private <T extends GeoAnimatable> PlayState predicate(AnimationState<T> tAnimationState) {
-        tAnimationState.getController().setAnimation(RawAnimation.begin().then("animation.platyplushie_block.bounce", Animation.LoopType.PLAY_ONCE));
-        return PlayState.CONTINUE;
+        controllers.add(new AnimationController<>(this, "bounce_controller", 0, state -> PlayState.STOP)
+                .triggerableAnim("bounce", BOUNCE));
     }
 
     @Override
